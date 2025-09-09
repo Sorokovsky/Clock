b6f65e0ad47e59f05e901034a93327ed09445f26
Sorokovsky <Sorokovskys@ukr.net>
2025-09-09 18:02:45 +0300

    seconds arrow

--- clock_presenter.py
+++ clock_presenter.py
@@ -1,5 +1,5 @@
 from turtle import *
-
+import time
 
 class ClockPresenter:
     def __init__(self, radius: int = 100):
@@ -9,7 +9,8 @@ class ClockPresenter:
         reset()
         pen()
         pensize(3)
-        speed(1)
+        speed(0)  # Set to fastest speed
+        tracer(0)  # Disable animation
         title("Годинник")
         setup(1000, 1000)
         bgcolor(self._background_color)
@@ -20,21 +21,19 @@ class ClockPresenter:
         self._draw_circle()
         self._draw_indicators()
         self._draw_arrows()
+        update()  # Update the screen to show all drawings
         exitonclick()
 
     def _draw_circle(self):
         goto(0, -100)
-        speed(0)
         pendown()
         color(self._color)
         fillcolor(self._color)
         circle(self._radius)
 
     def _draw_indicators(self):
-        speed(0)
         color(self._color)
         for i in range(0, 60):
-            speed(0)
             height = 10
             if i % 5 == 0:
                 height *= 2
@@ -46,20 +45,27 @@ class ClockPresenter:
             left(360 / 60)
 
     def _draw_arrows(self):
-        second_arrow_length = 30
+        second_arrow_length = 70
         minute_arrow_length = int(second_arrow_length / 2)
+
         penup()
         goto(0, 0)
         pendown()
         color(self._color)
-        forward(self._radius - minute_arrow_length)
+        forward(minute_arrow_length)
+        update()
+
+        tracer(1)
         while True:
-            for i in range (0, 60):
+            for i in range(4, 60):
                 penup()
                 goto(0, 0)
+                setheading(i * 360 / 60)
                 pendown()
                 color(self._color)
-                forward(self._radius - second_arrow_length)
+                forward(second_arrow_length)
+                update()
+                time.sleep(1)
                 color(self._background_color)
-                backward(self._radius - second_arrow_length)
-                left(360 / 60)
+                backward(second_arrow_length)
+                backward(second_arrow_length)
